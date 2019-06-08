631b7a77dca651a638284cdbf5fd60efaab99140
Spexon <46728234+Spexon@users.noreply.github.com>
2019-06-08 16:25:50 -0400

    Completed day 5, derailed from tutorial

--- src/Snake.java
+++ src/Snake.java
@@ -3,6 +3,9 @@ public class Snake {
   // In Java, Class Variables should be private so that only its methods can change them.
   private int centerX = 100;
   private int centerY = 382;
+  private boolean up = false;
+  private boolean down = false;
+  private boolean left = false;
   private boolean jumped = false;
 
   private int speedX = 0;
@@ -14,48 +17,97 @@ public class Snake {
     if (speedX < 0) {
       centerX += speedX;
     } else if (speedX == 0) {
-      System.out.println("Do not scroll the background.");
+      //System.out.println("Do not scroll the background.");
 
     } else {
-      if (centerX <= 150) {
+      if (centerX <= 684) {
         centerX += speedX;
       } else {
-        System.out.println("Scroll Background Here");
+       // System.out.println("Scroll Background Here");
       }
     }
 
     // Updates Y Position
 
-    if (centerY + speedY >= 382) {
-      centerY = 382;
+    if (centerY + speedY >= 513) {
+      centerY = 513;
+      up = true;
     } else {
       centerY += speedY;
+      up = false;
     }
+    
 
     // Handles Jumping
     if (jumped == true) {
       speedY += 1;
 
-      if (centerY + speedY >= 382) {
-        centerY = 382;
+      if (centerY + speedY >= 513) {
+        centerY = 513;
         speedY = 0;
         jumped = false;
       }
-
+    }
+    
+    // Prevents going beyond Y coordinate of 0 (top)
+    if (centerY + speedY <= 60) {
+      centerY = 61;
+      System.out.println("Dead");
+    }
+ // Prevents going beyond Y coordinate of  410 (bottom)
+    if (centerY + speedY >= 513) {
+      System.out.println("Dead");
     }
 
-    // Prevents going beyond X coordinate of 0
+    // Prevents going beyond X coordinate of 0 (left)
     if (centerX + speedX <= 60) {
       centerX = 61;
+      System.out.println("Dead");
     }
+    // Prevents going beyond X coordinate of 676 (right)
+    if (centerX + speedX >= 684) {
+      System.out.println("Dead");
+    }
+  }
+
+  public boolean isUp() {
+    return up;
+  }
+
+  public boolean isDown() {
+    return down;
+  }
+
+  public boolean isLeft() {
+    return left;
+  }
+
+  public void setUp(boolean up) {
+    this.up = up;
+  }
+
+  public void setDown(boolean down) {
+    this.down = down;
+  }
+
+  public void setLeft(boolean left) {
+    this.left = left;
   }
 
   public void moveRight() {
-    speedX = 6;
+    speedX = 4;
   }
 
   public void moveLeft() {
-    speedX = -6;
+    speedX = -4;
+  }
+  
+  public void moveUp() {
+    speedY = -4;
+  }
+  
+  public void moveDown() {
+    speedY = 4;
   }
 
   public void stop() {
